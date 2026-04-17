6436270f5db88bcab574fbae0dcd5ab0c2ce6b63
akmalropip <akmalropip@gmail.com>
2026-04-17 15:13:40 +0700

    menambahkan bubblesortarray

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -23,4 +23,32 @@ void input()
             Silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n===================================\n";
+    cout << "       Masukkan Elemen Array       \n";
+    cout << "===================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    }
 }
